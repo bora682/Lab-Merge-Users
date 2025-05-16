cd152b8af3016b21858eceee31f7a558c60613bc
Bora <bora@christinesmbp16.mynetworksettings.com>
2025-05-16 09:49:54 -0400

    Your commit message

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
+require('datejs');
 
+function combineUsers(...userArrays) {
+  const combinedObject = {
+    users: [],
+    merge_date: ""
+  };
 
+  for (let arr of userArrays) {
+    combinedObject.users.push(...arr);
+  }
+
+  const today = new Date();
+  combinedObject.merge_date = today.toString("M/d/yyyy");
+
+  return combinedObject;
+}
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
